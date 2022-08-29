f759652587ee056cd773ca0d169ba211227a10a2
Kathiresh C <110410029+Kathiresh1551@users.noreply.github.com>
2022-08-29 21:35:50 +0530

    Add files via upload

--- CalculateAvg.py
+++ CalculateAvg.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 "Write a program to calculate average of numbers. Get the numbers from user until user enters 0."
 number = 1
 count = 0
@@ -12,3 +13,19 @@ while (number != 0):
 if(count > 0): 
     print("Average is ", (total/(count)))
 
+=======
+"Write a program to calculate average of numbers. Get the numbers from user until user enters 0."
+number = 1
+count = 0
+total = 0
+while (number != 0):
+    number = int(input("enter: "))
+    if (number != 0):  #if number is not equals to 0 
+        total = total + number   #total carries the entered numbers
+        count += 1   #incrementing the count for each loop
+    else:
+        break # if entered number is 0 the loop is break
+if(count > 0): 
+    print("Average is ", (total/(count)))
+
+>>>>>>> refs/remotes/origin/main
