c7f7fc5fda9f8fa206f5f2b97402be18c89ee606
Kathiresh C <110410029+Kathiresh1551@users.noreply.github.com>
2022-10-04 16:26:52 +0530

    Add files via upload

---
+++ MaxNumberFromFile.py
@@ -0,0 +1,32 @@
+file1 = open("C:\\Users\itssm\python\Sayur\Home-work\Others\\FileTester.csv","r")
+arrList = file1.readlines()
+itemList = []
+priceList = []
+
+for index in range (0, len(arrList)):
+    arrListSpilt = arrList[index]
+    newlist = arrListSpilt.split(",")
+    itemList.append(newlist[0])
+    priceList.append(newlist[1])
+    
+
+priceList.pop(0)
+Costlyrate = max(priceList, key=int)
+CostlyItem = priceList.index(Costlyrate)
+print("The costliest dish is", itemList[CostlyItem+1],"with ₹",Costlyrate)
+
+
+'''
+in file1
+
+Item	Prize
+Vadai	8
+Coke	20
+Burger	80
+Coffee	10
+Pizza	100
+
+
+output
+The costliest dish is Pizza with ₹ 100
+'''
