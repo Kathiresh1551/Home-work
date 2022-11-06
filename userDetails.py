5d4b7a8a444e4a98f4f235eca0f376c819952510
Kathiresh C <110410029+Kathiresh1551@users.noreply.github.com>
2022-11-06 14:00:00 +0530

    Add files via upload

---
+++ userDetails.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/employeeDetails')
+def calculator():
+    return render_template('userDetails.html')
+
+@app.route('/employees')
+def employees():
+    emp = [
+        {"salutation": "Mr", "Name": "Kathiresh", "lastName": "C", "phoneNumber": "123456789", "occupation": "FrontEnd Developer"},
+        # {"salutation": "Miss", "Name": "Gayathri", "lastName": "Kathiresh", "phoneNumber": "789456123", "occupation": "BackEnd Developer"},
+    ]
+    return emp
+
+@app.route('/employee')
+def employee():
+    emp = {"salutation": "Mr", "Name": "Kathiresh", "lastName": "C", "phoneNumber": "123456789", "occupation": "FrontEnd Developer"}
+
+    return emp
+app.run()
